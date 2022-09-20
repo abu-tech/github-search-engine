6888d7b0791791412ee9e0b651a693ed75e9af59
Mohammed samir <abutech8801@gmail.com>
2022-09-20 23:36:12 +0530

    initial commit

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,38 @@
+import { useState,useEffect } from "react"
+import axios from 'axios'
+
+function UserResults() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async()=> {
+        const res = await axios.get("https://api.github.com/users", {
+            headers:{
+                Authorization: `token ${process.env.REACT_APP_GITHUB_API_TOKEN}`
+            }
+        })
+        setUsers(res.data);
+        setLoading(false);
+    }
+
+    if(!loading){
+        return (
+            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                {users.map((user) => (
+                    <h3>{user.login}</h3>))}
+            </div>
+          )
+    }
+    else{
+        return(
+            <div className="flex justify-center">
+                <button className="btn loading">loading</button>
+            </div>
+        )
+    }
+}
+
+export default UserResults
